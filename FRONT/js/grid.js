257a0c790908334bbc475900b8bbf22884809e95
Mateo Herrera <mateo.herrera.prof@gmail.com>
2022-04-28 14:57:35 -0500

    Added grid exercises

---
+++ FRONT/js/grid.js
@@ -0,0 +1,35 @@
+document.getElementById("div1").addEventListener("click", ()=>{
+    location.href="./grid/grid1.html"
+})
+
+document.getElementById("div2").addEventListener("click", ()=>{
+    location.href="./grid/grid2.html"
+})
+
+document.getElementById("div3").addEventListener("click", ()=>{
+    location.href="./grid/grid3.html"
+})
+
+document.getElementById("div4").addEventListener("click", ()=>{
+    location.href="./grid/grid4.html"
+})
+
+document.getElementById("div5").addEventListener("click", ()=>{
+    location.href="./grid/grid5.html"
+})
+
+document.getElementById("div6").addEventListener("click", ()=>{
+    location.href="./grid/grid6.html"
+})
+
+document.getElementById("div7").addEventListener("click", ()=>{
+    location.href="./grid/grid7.html"
+})
+
+document.getElementById("div8").addEventListener("click", ()=>{
+    location.href="./grid/grid8.html"
+})
+
+document.getElementById("div9").addEventListener("click", ()=>{
+    location.href="./grid/grid9.html"
+})
