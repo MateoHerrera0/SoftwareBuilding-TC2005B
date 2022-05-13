2fa34d91b1640cb01b9363335968a55b11e7e0df
G3RZZZ <73681300+G3RZZZ@users.noreply.github.com>
2022-05-13 00:04:22 -0500

    Complete Game

---
+++ Videogames/MatFrog/Assets/Scripts/Home.cs
@@ -0,0 +1,36 @@
+// Mateo Herrera 
+// Gerardo Gutierrez
+// Finish line
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+
+    public GameObject homeFrog;
+
+    private void OnEnable() 
+    {
+        homeFrog.SetActive(true);
+    }
+
+    private void OnDisable() 
+    {
+        homeFrog.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            enabled = true;
+
+            // Frogger frogger = other.GetComponent<Frogger>();
+            // frogger.gameObject.SetActive(false);
+            // frogger.Invoke(nameof(frogger.Respawn), 1f);
+
+            FindObjectOfType<GameManager>().HomeOccupied();
+        }
+    }
+}
