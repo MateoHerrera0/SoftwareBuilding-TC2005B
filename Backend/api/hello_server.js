b8d3a74bf243ee57588c7185fa5e9b4a6600a353
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2022-05-26 22:17:51 -0500

    servers

--- Backend/api/hello_server.js
+++ Backend/api/hello_server.js
@@ -2,6 +2,7 @@
 
 import express from 'express'
 import fs from 'fs'
+import mysql from 'mysql2/promise'
 
 const app =  express()
 const port = 3000
@@ -29,6 +30,48 @@ app.get('/api/hello', (req, res) => {
         res.send('hello')
     }
 })
+
+app.get('/api/users', async (req, res) => {
+    
+    let connection = null;
+
+    try
+    {
+        connection = await mysql.createConnection(
+        {
+            host:'172.29.192.1', 
+            user:'web1', 
+            password:'memelTacoDeSalchicha12', 
+            database: 'api_game_db'
+        })
+        
+        console.log("Connection stablished!")
+
+        const [rows, fields] = await connection.execute('select * from users');
+        
+        console.log(Object.keys(rows[0]))
+
+        for (const r of rows)
+        {
+            console.log(Object.values(r))
+        }
+
+        // console.log(rows)
+        res.json(rows)
+    }
+    catch(error)
+    {
+        console.log(error)
+    }
+    finally
+    {
+        if(connection!==null) 
+        {
+            connection.end()
+            console.log("Connection closed succesfully!")
+        }
+    }
+    })
 //
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
--- Backend/js/modules_3/hello_user.js
+++ Backend/js/modules_3/hello_user.js
@@ -1,6 +1,28 @@
-const hello = await fetch('http://localhost:3000/api/hello', {
+const hello = await fetch('http://localhost:3000/api/users', {
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
     method:'GET'
 })
 
-const message = await hello.text()
-console.log(message);
+const message = await hello.json()
+
+function loadTable(event){
+    let table = document.getElementById("table")
+    for (let i = 0; i < message.length; i++) {
+        const element = message[i];
+        let elements = Object.values(element)
+        const row = document.createElement('tr')
+        for (let j = 0; j < elements.length; j++) {
+            const values = elements[j];
+            const column = document.createElement('td')
+            column.appendChild(document.createTextNode(values))
+            row.appendChild(column)
+        }
+        table.appendChild(row)
+    }
+    event.preventDefault()
+}
+
+document.getElementById("button").addEventListener("click", loadTable)
